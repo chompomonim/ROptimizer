609db5be547e34c8537d963460423d67f33ee283
Jaro Šatkevič <jaro@akl.lt>
2012-02-06 02:08:34 +0200

    Show/hide javascript code.

--- src/roptimizer/static/js/scripts.js
+++ src/roptimizer/static/js/scripts.js
@@ -1,1 +1,30 @@
- 
+$(function() {
+
+    $('#show-incomes').click(function() {
+	if ($('#incomes').css('display') === 'none') {
+	    $('#incomes').show();
+	}
+	else {
+	    $('#incomes').hide();
+	}
+    });
+
+    $('#show-expenses').click(function() {
+	if ($('#expenses').css('display') === 'none') {
+	    $('#expenses').show();
+	}
+	else {
+	    $('#expenses').hide();
+	}
+    });
+
+    $('#show-periods').click(function() {
+	if ($('#periods').css('display') === 'none') {
+	    $('#periods').show();
+	}
+	else {
+	    $('#periods').hide();
+	}
+    });
+
+});
